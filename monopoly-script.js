a221852c59380ded307c9cad367e006be13f25b5
simGOATed <64333520+simGOATed@users.noreply.github.com>
2020-08-05 07:32:07 +0200

    Changes August 5
    -Added form data processing.
    ---Data is stored using sessionStorage.
    -Added raw multiplayer functionality.
    
    Be advised thatI did most of this while either half awake or only paying half attention 
    (sometimes both!) so you might bear witness to some of the worst structured code ever.
    
    It works tho (form might be slightly browser dependent but this should be a non-issue)

--- monopoly-script.js
+++ monopoly-script.js
@@ -1,21 +1,36 @@
-var pos = 0
 var spaces = 40;
+var turn = 0;
+var pos;
+var num;
 
+window.onload = function(){
+	var arr = sessionStorage.getItem("players");
+	var players = JSON.parse(arr);
+	num = players.length;
+	console.log(players);
+	pos = new Array(num)
+	for(i=0;i<num;i++){
+		pos[i] = 0;
+	}
+}
 
 function move(dice, doubled){
-	pos += dice;
-	pos %= spaces;
-	document.getElementById("b"+pos).appendChild(document.getElementById("piece1"));
+	pos[turn] += dice;
+	pos[turn] %= spaces;
+	console.log(turn+1)
+	document.getElementById("b"+pos[turn]).appendChild(document.getElementById("piece" + (turn+1)));
+	turn++;
+	turn%=num;
 }
 
 //Runs when a player rolls the dice
 function roll() {
-  var die1 = Math.floor(Math.random() * 6 + 1);
-  var die2 = Math.floor(Math.random() * 6 + 1);
-  document.getElementById("die1").innerHTML = die1;
-  document.getElementById("die2").innerHTML = die2;
-  dice = die1 + die2;
-  move(dice);
+	var die1 = Math.floor(Math.random() * 6 + 1);
+	var die2 = Math.floor(Math.random() * 6 + 1);
+	document.getElementById("die1").innerHTML = die1;
+	document.getElementById("die2").innerHTML = die2;
+	dice = die1 + die2;
+	move(dice);
 }
 
 // Just Trying stuff
--- welcome.js
+++ welcome.js
@@ -1,10 +1,48 @@
 // Variables
 var numPlayers = 0;
+var counter = 0;
 var playerNames = [];
 
 // Function that is called when a number of users is saved
 function initNumPlayers() {
-  numPlayers = document.getElementById("numPlayers").value;
+	numPlayers = document.getElementById("numPlayers").value;
+	switch(numPlayers){
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+			document.getElementById("numPlayers").disabled = true;
+			document.getElementById("save").disabled = true;
+			document.getElementById("playerOne").disabled = false;
+			document.getElementById("next").disabled = false;
+			break;
+		default:
+			alert("Please enter a number between 2 and 6")
+	}
+}
 
-  document.getElementById("numPlayers").disabled = true;
+function nextPlayer(){
+	player = document.getElementById("playerOne").value
+	if(player!=""){
+		playerNames.push(player);
+		counter++;
+		if(counter==numPlayers){
+			document.getElementById("playerOne").disabled = true;
+			document.getElementById("next").disabled = true;
+			document.getElementById("start").disabled = false;
+		}
+		else{
+			document.getElementById("player").innerHTML = "Player " + (counter+1) + ":";
+		}
+	}
+	else{
+		alert("Field cannot be empty")
+	}
+}
+
+function startGame(){
+	players = JSON.stringify(playerNames);
+	sessionStorage.setItem("players", players);
+	location.href = "monopoly.html"
 }
